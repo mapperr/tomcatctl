5cc520f63c1cd8916a43da8f692dbf276f4d6e8b
mapperr <michelangeloperrotta@gmail.com>
2014-09-09 18:07:48 +0200

    aggiornato web

--- web/index.php
+++ web/index.php
@@ -16,26 +16,28 @@ try{
 if(isset($_FILES['file']))
 {
 	$tempPos = $_FILES['file']['tmp_name'];
+	//$destPos = $tempPos;
 	$destPos = "/tmp/".$_FILES['file']['name'];
 	move_uploaded_file($tempPos, $destPos);
 	$istanza = $_POST['istanza'];
-	$command = "$tomcatctl_bin deploy $istanza $destPos";
-	if(isset($_POST['context']) && ($_POST['context'] != ""))
+	$command = "$tomcatctl_bin deploy $istanza " . '"' . "$destPos" . '"';
+	if(isset($_POST['context']) && (trim($_POST['context'])!==""))
 	{
 		$command .= " " . $_POST['context'];
-		if(isset($_POST['version']) && ($_POST['version'] != ""))
+		if(isset($_POST['version']) && (trim($_POST['version'])!==""))
 		{
 			$command .= " " . $_POST['version'];
 		}
 	}
-	exec("$command", $output_lines);
+	echo "<p>".$command."</p>";
+	exec($command, $output_lines);
 	echo "<div id='msg'><pre>";
 	foreach($output_lines as $outline) echo "$outline<br />";
 	echo "</pre></div>";
 	unlink($destPos);
 }
 
-if($_GET['cmd'] == "log")
+if(isset($_GET['cmd']) && $_GET['cmd'] == "log")
 {
 	$istanza = $_GET['istanza'];
 	exec("$tomcatctl_bin log $istanza cat", $output_lines);
@@ -46,7 +48,7 @@ if($_GET['cmd'] == "log")
 }
 
 // app restart
-if( $_GET['cmd'] == "restart" )
+if(isset($_GET['cmd']) && $_GET['cmd'] == "restart" )
 {
 	$istanza = $_GET['istanza'];
 	$context = $_GET['path'];
@@ -59,7 +61,7 @@ if( $_GET['cmd'] == "restart" )
 
 
 // app start
-if( $_GET['cmd'] == "start" )
+if(isset($_GET['cmd']) && $_GET['cmd'] == "start" )
 {
     $istanza = $_GET['istanza'];
     $context = $_GET['path'];
@@ -71,7 +73,7 @@ if( $_GET['cmd'] == "start" )
 }
 
 // app stop
-if( $_GET['cmd'] == "stop" )
+if( isset($_GET['cmd']) && $_GET['cmd'] == "stop" )
 {
     $istanza = $_GET['istanza'];
     $context = $_GET['path'];
@@ -82,7 +84,7 @@ if( $_GET['cmd'] == "stop" )
 	echo "</pre></div>";
 }
 
-if( $_GET['cmd'] == "reload" )
+if( isset($_GET['cmd']) && $_GET['cmd'] == "reload" )
 {
     $istanza = $_GET['istanza'];
     $context = $_GET['path'];
@@ -94,7 +96,7 @@ if( $_GET['cmd'] == "reload" )
 }
 
 
-if( $_GET['cmd'] == "undeploy" )
+if( isset($_GET['cmd']) && $_GET['cmd'] == "undeploy" )
 {
     $istanza = $_GET['istanza'];
     $context = $_GET['path'];
@@ -125,31 +127,36 @@ foreach($list_outlines as $line)
 		$style_fragment = "font-weight:bold;cursor:pointer;";
 	}
 	echo "<pre id='status_$codiceIstanza' $onclick_fragment style='font-size:20px;$style_fragment'>$line</pre>";
-	echo "<div id='apps_$codiceIstanza' style='margin-left:100px;display:none;'>";
-	echo "<a href='?cmd=log&amp;istanza=$codiceIstanza#lastlogline'>log</a> ";
-	echo <<<TT
-		<form name="deploy" method="post" action="" enctype="multipart/form-data"> 
-		<input type="hidden" name="istanza" value="$codiceIstanza">
-		war: <input type="file" name="file"> context path: <input type="text" name="context"> version: <input type="text" name="version"> <input type="submit" name="submit" value="deploy">
-		</form>
-TT;
-	echo "<pre>";
-	foreach($apps_outlines as $apps_line)
+	if($status)
 	{
-		$apps_words = preg_split("/[\s\t]+/",trim($apps_line));
-		$app_context = $apps_words[0];
-		$app_version = $apps_words[1];
-		$app_status = $apps_words[2];
-		$version_querystring = ($app_version != "##") ? "&amp;version=$app_version":"";
-		echo "<a href='?cmd=restart&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>restart</a> ";
-		echo "<a href='?cmd=start&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>start</a> ";
-		echo "<a href='?cmd=stop&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>stop</a> ";
-		echo "<a href='?cmd=reload&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>reload</a> ";
-		echo "<a href='?cmd=undeploy&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>undeploy</a> ";
-		echo "<a href='$host:90$codiceIstanza$app_context'>$app_context</a> $app_version $app_status\n";
+		echo "<div id='apps_$codiceIstanza' style='margin-left:100px;display:none;'>";
+		echo "<p><a href='?cmd=log&amp;istanza=$codiceIstanza#lastlogline'>LOG (catalina.out)</a></p>";
+		echo <<<TT
+			<form name="deploy" method="post" action="" enctype="multipart/form-data"> 
+			<input type="hidden" name="MAX_FILE_SIZE" value="50000000" />
+			<input type="hidden" name="istanza" value="$codiceIstanza"/>
+			war: <input type="file" name="file"/> context path: <input type="text" name="context"/> version: <input type="text" name="version"/> <input type="submit" name="submit" value="deploy"/>
+			</form>
+TT;
+		echo "<pre>";
+		foreach($apps_outlines as $apps_line)
+		{
+			$apps_words = preg_split("/[\s\t]+/",trim($apps_line));
+			$app_context = $apps_words[0];
+			$app_version = $apps_words[1];
+			$app_status = $apps_words[2];
+			$version_querystring = ($app_version != "##") ? "&amp;version=$app_version":"";
+			echo "<a href='?cmd=restart&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>restart</a> ";
+			echo "<a href='?cmd=start&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>start</a> ";
+			echo "<a href='?cmd=stop&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>stop</a> ";
+			echo "<a href='?cmd=reload&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>reload</a> ";
+			echo "<a href='?cmd=undeploy&amp;istanza=$codiceIstanza&amp;path=$app_context$version_querystring'>undeploy</a> ";
+			echo "<a href='$host:90$codiceIstanza$app_context'>$app_context</a> $app_version $app_status\n";
+		}
+		echo "</pre>";
+		echo "</div>";
 	}
-	echo "</pre>";
-	echo "</div></p>";
+	echo "</p>";
 }
 
 echo <<<TT
