973efaf2776409d7fb876dc6e3f2b0029d3351d7
perrotta <michele.perrotta@cabel.it>
2016-02-09 13:47:35 +0100

    removed shitty web interface

--- web/conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$tomcatctl_bin = "/data/tomcatctl/tomcatctl";
-$host = "http://192.168.0.76";
-
-?>
--- web/main.js
+++
@@ -1,6 +0,0 @@
-function toggle_info(codiceIstanza)
-{
-	if($("#apps_"+codiceIstanza).css("display") == "none") $("#apps_"+codiceIstanza).show("slow");
-	else $("#apps_"+codiceIstanza).hide("slow");
-}
-
